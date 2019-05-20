a9fe0d4cfee4a37faf85ba6ba27ad90060575c03
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2019-05-20 19:06:40 +0300

    DEX-288 Switch matching rules during work
    * OrderBook and OrderBookActor works with rules. Worked with normalizedTickSize before, thus wasn't able to preserve matching results after restart.
    * OrderBookActor is able to switch rules during the work.
    
    MatcherSettings:
    * "order-restrictions" now is a dictionary pair->rules (was a list) and used only during validation;
    * "matching-rules" are rules to update a functionality of matching engine. See the application.conf in DEX for further information;

--- dex/src/main/scala/com/wavesplatform/matcher/settings/OrderRestrictionsSettings.scala
+++ dex/src/main/scala/com/wavesplatform/matcher/settings/OrderRestrictionsSettings.scala
@@ -3,34 +3,30 @@ package com.wavesplatform.matcher.settings
 import cats.data.NonEmptyList
 import cats.implicits._
 import com.wavesplatform.settings.utils.ConfigSettingsValidator
-import com.wavesplatform.settings.utils.ConfigSettingsValidator.ErrorsListOr
-import com.wavesplatform.transaction.assets.exchange.AssetPair
-import com.wavesplatform.transaction.assets.exchange.AssetPair._
+import com.wavesplatform.settings.utils.ConfigSettingsValidator.{ErrorsListOr, _}
 import monix.eval.Coeval
 import net.ceedubs.ficus.Ficus._
 import net.ceedubs.ficus.readers.ValueReader
 import play.api.libs.json.{JsObject, Json}
-import com.wavesplatform.settings.utils.ConfigSettingsValidator._
 
-case class OrderRestrictionsSettings(stepSize: Double,
+case class OrderRestrictionsSettings(stepAmount: Double,
                                      minAmount: Double,
                                      maxAmount: Double,
-                                     tickSize: Double,
+                                     stepPrice: Double,
                                      minPrice: Double,
-                                     maxPrice: Double,
-                                     mergeSmallPrices: Boolean) {
+                                     maxPrice: Double) {
 
   import OrderRestrictionsSettings._
 
+  // TODO
   def getJson: Coeval[JsObject] = Coeval.evalOnce {
     Json.obj(
-      "stepSize"         -> formatValue(stepSize),
-      "minAmount"        -> formatValue(minAmount),
-      "maxAmount"        -> formatValue(maxAmount),
-      "tickSize"         -> formatValue(tickSize),
-      "minPrice"         -> formatValue(minPrice),
-      "maxPrice"         -> formatValue(maxPrice),
-      "mergeSmallPrices" -> mergeSmallPrices
+      "stepAmount" -> formatValue(stepAmount),
+      "minAmount"  -> formatValue(minAmount),
+      "maxAmount"  -> formatValue(maxAmount),
+      "stepPrice"  -> formatValue(stepPrice),
+      "minPrice"   -> formatValue(minPrice),
+      "maxPrice"   -> formatValue(maxPrice)
     )
   }
 }
@@ -43,7 +39,7 @@ object OrderRestrictionsSettings {
 
   def formatValue(value: Double): String = new java.text.DecimalFormat("#.########").format(value)
 
-  implicit val orderRestrictionsSettingsReader: ValueReader[(AssetPair, OrderRestrictionsSettings)] = { (cfg, path) =>
+  implicit val orderRestrictionsSettingsReader: ValueReader[OrderRestrictionsSettings] = { (cfg, path) =>
     val cfgValidator = ConfigSettingsValidator(cfg)
 
     def validateSizeMinMax(sizeSettingName: String,
@@ -64,17 +60,12 @@ object OrderRestrictionsSettings {
         .ensure(NonEmptyList(s"Required $minSettingName < $maxSettingName", Nil)) { case (_, min, max) => min < max }
     }
 
-    lazy val validateAssetPair        = cfgValidator.validate[AssetPair](s"$path.pair")
-    lazy val validateMergeSmallPrices = cfgValidator.validateWithDefault(s"$path.merge-small-prices", false)
-
     (
-      validateAssetPair,
-      validateSizeMinMax(s"$path.step-size", s"$path.min-amount", s"$path.max-amount", stepSizeDefault, minAmountDefault, maxAmountDefault),
-      validateSizeMinMax(s"$path.tick-size", s"$path.min-price", s"$path.max-price", tickSizeDefault, minPriceDefault, maxPriceDefault),
-      validateMergeSmallPrices
+      validateSizeMinMax(s"$path.step-amount", s"$path.min-amount", s"$path.max-amount", stepSizeDefault, minAmountDefault, maxAmountDefault),
+      validateSizeMinMax(s"$path.step-price", s"$path.min-price", s"$path.max-price", tickSizeDefault, minPriceDefault, maxPriceDefault),
     ) mapN {
-      case (assetPair, (stepSize, minAmount, maxAmount), (tickSize, minPrice, maxPrice), mergeSmallPrices) =>
-        assetPair -> OrderRestrictionsSettings(stepSize, minAmount, maxAmount, tickSize, minPrice, maxPrice, mergeSmallPrices)
+      case ((stepSize, minAmount, maxAmount), (tickSize, minPrice, maxPrice)) =>
+        OrderRestrictionsSettings(stepSize, minAmount, maxAmount, tickSize, minPrice, maxPrice)
     } getValueOrThrowErrors
   }
 }
